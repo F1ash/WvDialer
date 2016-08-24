d24babfd4acf27f712fe928ff5287223c9e611e2
F1ash <kaperang07@gmail.com>
2016-08-24 19:35:13 +0300

    prepared methods for use transient systemd unit; some fixes;

--- src/helper/wvdialer_helper.cpp
+++ src/helper/wvdialer_helper.cpp
@@ -2,12 +2,14 @@
 #include "wvdialer_helper.h"
 #include <signal.h>
 
+#define WVDIALER QString("wvdialer")
+
 WvDialHelper::WvDialHelper(QObject *parent) :
     QObject(parent)
 {
 }
 
-QString WvDialHelper::get_key_varmap(const QVariantMap &args, const QString& key)
+QString WvDialHelper::get_key_varmap(const QVariantMap &args, const QString& key) const
 {
     QString value;
     if (args.keys().contains(key)) {
@@ -18,11 +20,11 @@ QString WvDialHelper::get_key_varmap(const QVariantMap &args, const QString& key
     return value;
 }
 
-ActionReply WvDialHelper::run(const QVariantMap args)
+ActionReply WvDialHelper::run(const QVariantMap args) const
 {
     ActionReply reply;
  
-    QString act = get_key_varmap(args, "action");
+    const QString act = get_key_varmap(args, "action");
     if ( act!="run" ) {
         QVariantMap err;
         err["result"] = QString::number(-1);
@@ -45,11 +47,11 @@ ActionReply WvDialHelper::run(const QVariantMap args)
     return reply;
 }
 
-ActionReply WvDialHelper::kill(const QVariantMap args)
+ActionReply WvDialHelper::kill(const QVariantMap args) const
 {
     ActionReply reply;
 
-    QString act = get_key_varmap(args, "action");
+    const QString act = get_key_varmap(args, "action");
     if ( act!="kill" ) {
         QVariantMap err;
         err["result"] = QString::number(-1);
@@ -73,6 +75,114 @@ ActionReply WvDialHelper::kill(const QVariantMap args)
 
     QVariantMap retdata;
     retdata["code"] = QString::number(code);
+
+    reply.setData(retdata);
+    return reply;
+}
+
+ActionReply WvDialHelper::create(const QVariantMap args) const
+{
+    ActionReply reply;
+
+    const QString act = get_key_varmap(args, "action");
+    if ( act!="create" ) {
+        QVariantMap err;
+        err["result"] = QString::number(-1);
+        reply.setData(err);
+        return reply;
+    };
+
+    // Start new transient service unit: wvdialer.service
+    QProcess proc;
+    proc.setProgram("/usr/bin/systemd-run");
+    proc.setArguments(QStringList()
+                      <<"unit"<<WVDIALER
+                      <<"service-type"<<"forking"
+                      <<"/usr/bin/wvdial");
+    proc.start();
+    proc.waitForFinished();
+
+    QVariantMap retdata;
+    retdata["code"] = QString::number(proc.exitCode());
+
+    reply.setData(retdata);
+    return reply;
+}
+
+ActionReply WvDialHelper::start(const QVariantMap args) const
+{
+    ActionReply reply;
+
+    const QString act = get_key_varmap(args, "action");
+    if ( act!="start" ) {
+        QVariantMap err;
+        err["result"] = QString::number(-1);
+        reply.setData(err);
+        return reply;
+    };
+
+    QProcess proc;
+    proc.setProgram("/usr/bin/systemctl");
+    proc.setArguments(QStringList()<<act<<WVDIALER);
+    proc.start();
+    proc.waitForFinished();
+
+    QVariantMap retdata;
+    retdata["code"] = QString::number(proc.exitCode());
+
+    reply.setData(retdata);
+    return reply;
+}
+
+ActionReply WvDialHelper::status(const QVariantMap args) const
+{
+    ActionReply reply;
+
+    const QString act = get_key_varmap(args, "action");
+    if ( act!="is-active" ) {
+        QVariantMap err;
+        err["result"] = QString::number(-1);
+        reply.setData(err);
+        return reply;
+    };
+
+    QProcess proc;
+    proc.setProgram("/usr/bin/systemctl");
+    proc.setArguments(QStringList()<<act<<WVDIALER);
+    proc.start();
+    proc.waitForReadyRead();
+    // 'inactive' -- service not exist
+    // 'active'   -- service exist and running
+    // 'failed'   -- service exist and stopped
+    QByteArray result = proc.readAll();
+
+    QVariantMap retdata;
+    retdata["result"] = QString::fromUtf8(result.data());
+
+    reply.setData(retdata);
+    return reply;
+}
+
+ActionReply WvDialHelper::stop(const QVariantMap args) const
+{
+    ActionReply reply;
+
+    const QString act = get_key_varmap(args, "action");
+    if ( act!="stop" ) {
+        QVariantMap err;
+        err["result"] = QString::number(-1);
+        reply.setData(err);
+        return reply;
+    };
+
+    QProcess proc;
+    proc.setProgram("/usr/bin/systemctl");
+    proc.setArguments(QStringList()<<act<<WVDIALER);
+    proc.start();
+    proc.waitForFinished();
+
+    QVariantMap retdata;
+    retdata["code"] = QString::number(proc.exitCode());
 
     reply.setData(retdata);
     return reply;
--- src/helper/wvdialer_helper.h
+++ src/helper/wvdialer_helper.h
@@ -8,9 +8,14 @@ public:
     explicit WvDialHelper(QObject *parent = nullptr);
 
 public slots:
-    ActionReply     run(const QVariantMap args);
-    ActionReply     kill(const QVariantMap args);
+    ActionReply     run(const QVariantMap args) const;
+    ActionReply     kill(const QVariantMap args) const;
+
+    ActionReply     create(const QVariantMap args) const;
+    ActionReply     start(const QVariantMap args) const;
+    ActionReply     status(const QVariantMap args) const;
+    ActionReply     stop(const QVariantMap args) const;
 
 private:
-    QString         get_key_varmap(const QVariantMap &args, const QString& key);
+    QString         get_key_varmap(const QVariantMap &args, const QString& key) const;
 };
